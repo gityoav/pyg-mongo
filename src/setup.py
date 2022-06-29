14a7eba2f47424a6fab1fbe15938ca1bb98b4e34
gityoav <30290427+gityoav@users.noreply.github.com>
2022-06-29 21:07:50 +0100

    v12

--- src/setup.py
+++ src/setup.py
@@ -4,4 +4,4 @@ import platform
 from setuptools import setup, find_packages
 from distutils.core import Extension
 
-setup(name = 'pyg-mongo', version = '0.0.11', packages = find_packages(), python_requires = '>=3.7.')
+setup(name = 'pyg-mongo', version = '0.0.12', packages = find_packages(), python_requires = '>=3.7.')
